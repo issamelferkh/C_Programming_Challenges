6864e1295b698c1d977b2f12e16328dcf09566a1
issamelferkh <issam.elferkh@gmail.com>
2020-01-06 08:39:04 +0100

    fix all issues in rev_wstr

--- rev_wstr/rev_wstr.c
+++ rev_wstr/rev_wstr.c
@@ -6,35 +6,63 @@
 /*   By: iel-ferk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 20:44:13 by iel-ferk          #+#    #+#             */
-/*   Updated: 2020/01/05 17:44:33 by iel-ferk         ###   ########.fr       */
+/*   Updated: 2020/01/06 08:38:03 by iel-ferk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Write Prog: print string reversed taled as a parameter
-// Word => space || tab
-// OK			if argc != 2 => print '\n'
-// Input => Str with spaces at the beginning and end 
-// Output => Str rev with exactly one space
-//
-// Issue => if space in the beg of the str -> print space !
-
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char*	ft_strrev(char *str) {
+	char *tmp1;
+	int i = 0, j = 0;
+
+	while (str[i])
+		i++;
+	i--;
+	tmp1 = (char*)malloc(sizeof(char) * i + 1);
+	while (str[i]) {
+		tmp1[j] = str[i];
+		i--;
+		j++;
+	}
+	tmp1[j] = '\0';
+	return (tmp1);
+}
+
+char*	ft_strtrim(char *str) {
+	char *tmp2;
+	int i = 0, j = 0;
+
+	while (str[i])
+		i++;
+	tmp2 = (char*)malloc(sizeof(char) * i);
+	i = 0;
+	while (str[i]) {
+		if ((str[i]== ' ' || str[i] == '\t') && j == 0) {
+			i++;
+		}
+		else {
+			tmp2[j] = str[i];
+			i++;
+			j++;
+		}
+	}
+	tmp2[j] = '\0';
+	return (tmp2);
+}
 
 int	ft_putlast(char *str, int len) {
 	int i = 0;
 
 	while (len > 0 && (str[len] == ' ' || str[len] == '\t'))
 		len--;
-	//printf("len1 est ===> %d\n",len);
 	while (i <= len && str[len-i] != ' ' && str[len-i] != '\t')
 		i++;
-	//printf("i    est ===> %d\n",i);
 	write (1,str+len-i+1,i);
-	//write(1,"\n",1);
 	while (len > 0 && (str[len] == ' ' || str[len] == '\t'))
 		len--;
-	//printf("len2 est ===> %d\n",len);*/
 	if (i < len)
 		write (1," ",1);
 	return (len-i);
@@ -45,10 +73,13 @@ int main(int ac, char **av) {
 	int len = 0;
 
 	if (ac == 2) {
+		av[1] = ft_strtrim(av[1]);
+		av[1] = ft_strrev(av[1]);
+		av[1] = ft_strtrim(av[1]);
+		av[1] = ft_strrev(av[1]);
 		while (av[1][len])
 			len++;
 		len--;
-		//printf("len0 est ===> %d\n",len);
 		while (len > 0)
 			len = ft_putlast(av[1], len);
 	}
