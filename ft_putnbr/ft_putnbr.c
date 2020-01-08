4309e958f8a6dda49280c68cfef5862f91e63f13
issamelferkh <issam.elferkh@gmail.com>
2020-01-08 15:21:02 +0100

    fix ft_putnbr

--- ft_putnbr/ft_putnbr.c
+++ ft_putnbr/ft_putnbr.c
@@ -11,25 +11,31 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h> // for test
 
 void	ft_putchar(char c) {
 	write (1,&c,1);
 }
 
 void    ft_putnbr(int n) {
-	char c;
-	
-	while (n) {
-		c = n % 10 + 48;
-		ft_putchar(c);
-		n = n / 10;
-		if (n > 9)
-			ft_putnbr(n);
+	if (n == -2147483648) {
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	} else if (n < 0) {
+		ft_putchar('-');
+		ft_putnbr(-n);
+	} else if(n > 9) {
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	} else {
+		ft_putchar(n + 48);
 	}
-	write(1,"\n",1);
+	return ;
 }
 
-int main() {
-	ft_putnbr(4512);
+int main() // for test 
+{
+	ft_putnbr(-2147483648);
 	return (0);
 }
