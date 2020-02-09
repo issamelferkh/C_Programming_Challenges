0a2bb52f033abb423bc8f423b3e139364fc11fe4
issamelferkh <issam.elferkh@gmail.com>
2020-02-09 21:59:12 +0100

    add check_mate OK

--- check_mate/check_mate.c
+++ check_mate/check_mate.c
@@ -10,119 +10,106 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-int     ft_strlen(char *str) {
-    int i = 0;
-    while (str[i])
-        i++;
-    return (i);
+#include "check_mate.h"
+
+int	ft_strlen(char *str) {
+	int i = 0;
+	while (str[++i]) {}
+	return (i);
 }
 
-int ft_piece(char c){
-    return (c == 'P' || c == 'R' || c == 'B' || c == 'Q');
+int	ft_piece(char c) {
+	return (c == 'Q' || c == 'R' || c == 'B' || c == 'P');
 }
 
-int ft_pawn(char **av, int y, int x) {
-    return((av[y-1][x-1] && av[y-1][x-1] == 'K') || (av[y-1][x+1] && av[y-1][x+1] == 'K'));
+int	ft_pawn(char **av, int j, int i) {
+	if ((av[j-1][i-1] == 'K' && av[j-1][i-1]) || (av[j-1][i+1] == 'K' && av[j-1][i+1]))
+		return (1);
+	return (0);
 }
 
-int	ft_rook(char **board, int y, int x)
-{
-	int	len;
-	int	j;
-
-	len = (int)ft_strlen(board[y]);
-	j = x;
-	while (++j < len && ft_piece(board[y][j]) != 1) //Horizontal++
-	{
-		if (board[y][j] == 'K')
-			return (1);
-	}
-	j = x;
-	while (--j >= 0 && ft_piece(board[y][j]) != 1) //Horizontal--
-	{
-		if (board[y][j] == 'K')
+int	ft_rook(char **av, int j, int i) {
+	int y = j, x = i, len = ft_strlen(av[1]);
+	while (++i < len && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
 	j = y;
-	while (++j <= len && ft_piece(board[j][x]) != 1) //Vertical--
-	{
-		if (board[j][x] == 'K')
+	i = x;
+	while (--i >= 0 && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
 	j = y;
-	while (--j >= 1 && ft_piece(board[j][x]) != 1) //Vertical++
-	{
-		if (board[j][x] == 'K')
+	i = x;
+	while (++j <= len && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
+			return (1);
+	}
+	j = y;
+	i = x;
+	while (--j > 0 && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
 	return (0);
 }
 
-int	ft_bishop(char **board, int y, int x)
-{
-	int	len;
-	int	i;
-	int	j;
-
-	len = (int)ft_strlen(board[1]);
-	i = y;
-	j = x;
-	while (++i <= len && ++j < len && ft_piece(board[i][j]) != 1) //Down Right
-	{
-		if (board[i][j] == 'K')
+int	ft_bishop(char **av, int j, int i) {
+	int y = j, x = i, len = ft_strlen(av[1]);
+	while (++i < len && ++j <= len && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
-	i = y;
-	j = x;
-	while (--i >= 1 && --j >= 0 && ft_piece(board[i][j]) != 1) //Down Left
-	{
-		if (board[i][j] == 'K')
+	j = y;
+	i = x;
+	while (++i < len && --j > 0 && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
-	i = y;
-	j = x;
-	while (--i >= 1 && ++j < len && ft_piece(board[i][j]) != 1) //Up Right
-	{
-		if (board[i][j] == 'K')
+	j = y;
+	i = x;
+	while (--i >= 0 && ++j <= len && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
-	i = y;
-	j = x;
-	while (--i >= 1 && --j >= 0 && ft_piece(board[i][j]) != 1) //Up Left
-	{
-		if (board[i][j] == 'K')
+	j = y;
+	i = x;
+	while (--i >= 0 && --j > 0 && !ft_piece(av[j][i])) {
+		if (av[j][i] == 'K')
 			return (1);
 	}
 	return (0);
 }
 
-int ft_check_mate(char **av) {
-    int i = 0, j = 0;
+int	ft_check_mate(char **av) {
+	int j = 1, i;
 
-    while (av[j]) {
-        i = 0;
-        while (av[j][i]) {
-            if (av[j][i] == 'P' && ft_pawn(av, j, i))
-                return (1);
-            if (av[j][i] == 'R' && ft_rook(av, j, i))
-                return (1);
-            if (av[j][i] == 'B' && ft_bishop(av, j, i))
-                return (1);
-            if (av[j][i] == 'Q' && (ft_bishop(av, j, i) || ft_rook(av, j, i)))
-                return (1);
-            i++;
-        }
-        j++;
-    }
-    return (0);
+	while (av[j]) {
+		i = 0;
+		while (av[j][i]) {
+			if (av[j][i] == 'P' && ft_pawn(av,j,i))
+				return (1);
+			if (av[j][i] == 'R' && ft_rook(av,j,i))
+				return (1);
+			if (av[j][i] == 'B' && ft_bishop(av,j,i))
+				return (1);
+			if (av[j][i] == 'Q' && (ft_rook(av,j,i) || ft_bishop(av,j,i)))
+				return (1);
+			i++;
+		}
+		j++;
+	}
+	return (0);
 }
 
 int main (int ac, char **av) {
-    if(ac > 1) {
-        ft_check_mate(av)?write(1,"Success",7):write(1,"Fail",4);
-    }
-    write(1,"\n",1);
-    return (0);
+	if (ac > 1) {
+		if (ft_check_mate(av))
+			write(1,"Success",7);
+		else
+			write(1,"Fail",4);
+	}
+	write(1,"\n",1);
+	return (0);
 }
